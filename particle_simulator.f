44c5a47f3872aad0fe4866a4d7eee8febf7c5f3d
Ravon Venters <ray12514@gmail.com>
2017-12-09 08:42:38 -0500

    Checking Particles agian

--- particle_simulator.f
+++ particle_simulator.f
@@ -168,8 +168,8 @@ c
       real  xdrange(2,3) 
       common /domainrange/ xdrange
       save partdiam,partdens
-      data partdiam /1e-6,1e-3,1e-3/
-      data partdens /1000,2,100/
+      data partdiam /1e-6,1e-4,1e-3/
+      data partdens /2,2,2/
 
       integer lcount,icount
       save    lcount,icount
